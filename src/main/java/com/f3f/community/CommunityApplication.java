5afde813301f28ad6ade90844aa9bd0ffa035d9e
ryudongjae <rdj10149@gmail.com>
2022-04-16 00:40:46 +0900

    first commit

--- src/main/java/com/f3f/community/CommunityApplication.java
+++ src/main/java/com/f3f/community/CommunityApplication.java
@@ -2,7 +2,7 @@ package com.f3f.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test
+
 @SpringBootApplication
 public class CommunityApplication {
 
