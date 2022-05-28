6125222d9000e330ad407afb60facb92d282ab25
ryudongjae <rdj10149@gmail.com>
2022-05-28 14:15:11 +0900

    jpa 추가

--- src/main/java/com/f3f/community/comment/domain/Comment.java
+++ src/main/java/com/f3f/community/comment/domain/Comment.java
@@ -1,4 +1,19 @@
 package com.f3f.community.comment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
+
+    @Id@GeneratedValue
+    private Long id;
 }
--- src/main/java/com/f3f/community/comment/repository/CommentRepository.java
+++ src/main/java/com/f3f/community/comment/repository/CommentRepository.java
@@ -1,9 +1,10 @@
-package com.f3f.community.admin.repository;
+package com.f3f.community.comment.repository;
 
 import com.f3f.community.admin.domain.Admin;
+import com.f3f.community.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin,Long> {
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 }
