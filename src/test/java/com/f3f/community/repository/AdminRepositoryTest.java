4b32e16171e0973ab98c448f90c94ff6f7e5f67d
ryudongjae <rdj10149@gmail.com>
2022-06-18 16:39:07 +0900

    [ADD] 테스트 코드 구성 ,  README 테스트 코드 convention 추가

---
+++ src/test/java/com/f3f/community/repository/AdminRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.f3f.community.repository;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AdminRepositoryTest {
+
+
+    @DisplayName(value = "테스트 예제")
+    @Test
+    void test(){
+        //given
+
+        //when
+
+        //then
+    }
+}
---
+++ src/test/java/com/f3f/community/service/CommentServiceTest.java
@@ -0,0 +1,9 @@
+package com.f3f.community.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class CommentServiceTest {
+
+}
