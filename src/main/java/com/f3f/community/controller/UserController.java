abaeb17dd3870736f47168b10602c0966a7f5d9a
ryudongjae <rdj10149@gmail.com>
2022-07-25 22:05:02 +0900

    [ADD] controller

--- src/main/java/com/f3f/community/controller/UserController.java
+++ src/main/java/com/f3f/community/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.f3f.community.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
-
 
 }
