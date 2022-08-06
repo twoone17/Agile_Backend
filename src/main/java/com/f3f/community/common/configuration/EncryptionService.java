19521f20633ce8bd2d5b7eba8eb2f7a54cb67fdf
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-08-06 18:36:57 +0900

    [REFACTOR]post 피드백 반영 Exception 처리 전까지

--- src/main/java/com/f3f/community/common/configuration/EncryptionService.java
+++ src/main/java/com/f3f/community/common/configuration/EncryptionService.java
@@ -1,5 +1,5 @@
-package com.f3f.community.common.configuration;
-
-public interface EncryptionService {
-    String encrypt(String s);
-}
+//package com.f3f.community.common.configuration;
+//
+//public interface EncryptionService {
+//    String encrypt(String s);
+//}
--- src/main/java/com/f3f/community/common/configuration/Intercepter/LoginCheckIntercepter.java
+++ src/main/java/com/f3f/community/common/configuration/Intercepter/LoginCheckIntercepter.java
@@ -1,24 +1,24 @@
-package com.f3f.community.common.configuration.Intercepter;
-
-import com.f3f.community.user.service.SessionLoginService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Component
-@RequiredArgsConstructor
-public class LoginCheckIntercepter implements HandlerInterceptor {
-
-    private final SessionLoginService sessionLoginService;
-
-    private Environment environment;
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
-    }
-}
+//package com.f3f.community.common.configuration.Intercepter;
+//
+//import com.f3f.community.user.service.SessionLoginService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.core.env.Environment;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class LoginCheckIntercepter implements HandlerInterceptor {
+//
+//    private final SessionLoginService sessionLoginService;
+//
+//    private Environment environment;
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        return true;
+//    }
+//}
