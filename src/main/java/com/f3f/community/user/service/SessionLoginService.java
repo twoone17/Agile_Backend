30ce7be73fd78eb25f46c9fc50b71430a8b9696e
ryudongjae <rdj10149@gmail.com>
2022-08-06 16:27:21 +0900

    [ADD] 로그인 로직 의존성 해제

--- src/main/java/com/f3f/community/user/service/SessionLoginService.java
+++ src/main/java/com/f3f/community/user/service/SessionLoginService.java
@@ -1,48 +1,48 @@
-//package com.f3f.community.user.service;
+package com.f3f.community.user.service;
+
+
+import com.f3f.community.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Service
+@RequiredArgsConstructor
+public class SessionLoginService {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+   // private final EncryptionService encryptionService;
+
+
+//    /**
+//     * 아이디 비밀번호 일치 여부
+//     * @param loginRequest
+//     */
+//    @Transactional(readOnly = true)
+//    public void existByEmailAndPassword(UserDto.LoginRequest loginRequest) {
+//    }
+//    @Transactional(readOnly = true)
+//    public void login(UserDto.LoginRequest request) {
 //
-//import com.f3f.community.configuration.EncryptionService;
-//import com.f3f.community.user.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
+//    }
 //
-//import javax.servlet.http.HttpSession;
+//    public void setUserLevel(String email){
+//    }
 //
-//@Service
-//@RequiredArgsConstructor
-//public class SessionLoginService {
+//    public void logout() {
+//    }
 //
-//    private final UserRepository userRepository;
-//    private final HttpSession httpSession;
-//    private final EncryptionService encryptionService;
+//    public String getLoginUser() {
+//    }
 //
 //
-////    /**
-////     * 아이디 비밀번호 일치 여부
-////     * @param loginRequest
-////     */
-////    @Transactional(readOnly = true)
-////    public void existByEmailAndPassword(UserDto.LoginRequest loginRequest) {
-////    }
-////    @Transactional(readOnly = true)
-////    public void login(UserDto.LoginRequest request) {
-////
-////    }
-////
-////    public void setUserLevel(String email){
-////    }
-////
-////    public void logout() {
-////    }
-////
-////    public String getLoginUser() {
-////    }
-////
-////
-////    public UserDto.UserInfoDto getCurrentUser(String email){
-////
-////    }
-////
-////    private void banCheck(User user) {
-////    }
+//    public UserDto.UserInfoDto getCurrentUser(String email){
 //
-//}
+//    }
+//
+//    private void banCheck(User user) {
+//    }
+
+}
