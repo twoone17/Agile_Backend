e499233ddeec94cc62e41ef1d566bf54f345318b
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-08-06 19:02:17 +0900

    [REFACTOR] 도메인 아이디 생성 전략 리팩터링

--- src/main/java/com/f3f/community/category/domain/Category.java
+++ src/main/java/com/f3f/community/category/domain/Category.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
     @Column(name = "category_id")
     private Long id;
 
